d02dbe779c006c5ed66f899579632a0f5a8dc9fe
ianmax32 <masagaian32@gmail.com>
2022-06-02 13:54:02 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+/**
+ *main- printing a string inside a put function
+ *
+ *description: this program uses the put function to print
+ * "Programming is like building a multilingual puzzle
+ * Return: 0
+ */
 
 int main(void)
 {
