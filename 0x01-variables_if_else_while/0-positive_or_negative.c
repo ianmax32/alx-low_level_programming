a152dfed33baac5917553160da49b5ad6f82f9b9
ianmax32 <masagaian32@gmail.com>
2022-06-03 15:06:56 -0700

    Positive anything is better than nagtive nothing

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - this program assigns random numbers and prints them
+ * Description: the assigned number in the variable is displayed as 
+ * a positive or nagative.
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	if (n < 0)
+	{
+		printf("%i is nagative\n", n);
+	}
+	else if (n > 0)
+	{
+		printf("%i is positive\n", n);
+	}
+	else if (n == 0)
+	{
+		printf("%i is zero\n", n);
+	}
+	
+	return (0);
+}
