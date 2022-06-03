798a9f45a22fbe05e45fa461c4ad7b080715825e
ianmax32 <masagaian32@gmail.com>
2022-06-02 17:22:32 -0700

    UNIX is basically a simple operating system, but you have to be a genius to understand the simplicity

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+
+/**
+ * main - this is a program that printsa string without using
+ * printf or puts.
+ *
+ * Description: the function should print and that piece of art is
+ * useful"-Dora Korpar, 2015-10-19
+ * Return 1
+ */
+
+int main(void)
+{
+	write(1,"and that piece of art is useful\" -Dora Korpar, 2015-10-19\n",11);
+	return (1);
+}
