bde0eac773264ea75a908c0da619813459e5b909
ianmax32 <masagaian32@gmail.com>
2022-06-03 16:38:28 -0700

    Patience, persistence and perspiration make an unbeatable combination for success

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 /**
- * main - printing single digit numbers seperated by commas and space between them
+ * main - printing single digit numbers seperated by commas and
+ * space between them
  * Description: using the main function
- * this program prints "0, 1, 2, 3, 4, 5, 6, 7, 8, 9" using putchar function
+ * this program prints "0, 1, 2, 3, 4, 5, 6, 7, 8, 9" using
+ * putchar function
  * Return: 0
  */
 int main(void)
@@ -18,5 +20,5 @@ for (c = 0; c < 10; c++)
 	}
 }
 putchar('\n');
-return 0;
+return (0);
 }
