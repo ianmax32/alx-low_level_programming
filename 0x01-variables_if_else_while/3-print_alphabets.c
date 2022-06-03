30b7e593e5fa5f72334023d318912f7b3a727f96
ianmax32 <masagaian32@gmail.com>
2022-06-03 15:47:25 -0700

    alphABET

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -13,11 +13,11 @@ int main(void)
 char a;
 for (a = 'a' ; a <= 'z' ; a++)
 {
-        putchar(a);
+	putchar(a);
 }
 for (a = 'A' ; a <= 'Z' ; a++)
 {
-        putchar(a);
+	putchar(a);
 }
 putchar('\n');
 return (0);
