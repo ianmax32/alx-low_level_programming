8427582542bf5aecb829ee09bead08f936d8088d
ianmax32 <masagaian32@gmail.com>
2022-06-04 10:06:42 -0700

    Inventing is a combination of brains and materials. The more brains you use, the less material you need

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ * main - this program prints possible combinations of two digits
+ *
+ * Description: the numbers printed must be seperated by , and the
+ * numbers must be different, only the smallest combination must be 
+ * printed in ascending order using the putchar function
+ * Return: 0
+ */
+
+int main(void)
+{
+	int a = 0;
+	int b;
+	int c;
+
+	while(a < 99)
+	{
+		b = ((a / 10) + '0');
+		c = ((a % 10) + '0');
+
+		if(b < c)
+		{
+			putchar(b);
+			putchar(c);
+
+			if(a != 89)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+		a++;
+	}
+	putchar('\n');
+	return (0);
+}
