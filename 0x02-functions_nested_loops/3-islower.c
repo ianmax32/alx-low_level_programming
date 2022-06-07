bcde95ca299e6e5b93b889ee1b3ab463c22de550
ianmax32 <masagaian32@gmail.com>
2022-06-07 14:27:44 -0700

    islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,6 +3,7 @@
 /**
  * _islower - method returns 1 is c is lower or
  * o if not.
+ * @c : this is an int argument to be used in the function
  *Description: return either 1 is lower or 0 if not
  * Return: 0
  */
