3ff05a398250884cf79252c505a1d528a2261380
ianmax32 <masagaian32@gmail.com>
2022-06-07 14:58:59 -0700

    isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -3,14 +3,14 @@
 /**
  * _isalpha - method returns 1 is c is lower or
  * o if not.
- * @c : variable to be checked
+ * @c : argument varible to be used in the function
  *Description: return either 1 is lower or 0 if not
  * Return: 0
  */
 
 int _isalpha(int c)
 {
-	if (c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z')
+	if (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
         {
 		return (1);
 	 }
