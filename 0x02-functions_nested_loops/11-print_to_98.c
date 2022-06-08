b21135e47533153b93fa570cba066da47c57b519
ianmax32 <masagaian32@gmail.com>
2022-06-08 16:51:38 -0700

    98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,21 +1,20 @@
 #include <stdio.h>
 
 /**
- * print_to_98 - method prints natural numbers 
- * up to 98
- * @n: starting value as an argument to the function
- * Return:0
+ *print_to_98 - method prints natural numbers up to 98.
+ *@n: starting value as an argument to the function
+ *Return:0
  */
 
 void print_to_98(int n)
 {
 	int a;
 
-	if ( n <= 98)
+	if (n <= 98)
 	{
 		for (a = n ; a <= 98; a++)
 		{
-			printf("%d",a);
+			printf("%d", a);
 			if (a != 98)
 			{
 				printf(", ");
@@ -24,9 +23,9 @@ void print_to_98(int n)
 	}
 	else if (n > 98)
 	{
-		for (a = n; a >=98; a--)
+		for (a = n; a >= 98; a--)
 		{
-			printf("%d",a);
+			printf("%d", a);
 			if (a != 98)
 			{
 				printf(", ");
@@ -34,4 +33,4 @@ void print_to_98(int n)
 		}
 	}
 	printf("\n");
-}	
+}
