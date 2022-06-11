24a257bdd27db916e215e8a8525d72ef7077665c
ianmax32 <masagaian32@gmail.com>
2022-06-10 19:30:11 -0700

    Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -17,18 +17,19 @@ int main(void)
 	{
 		if (a % 3 == 0)
 		{
-			printf("%s ", "Fizz");
+			printf("%s ","Fizz");
 		}
 		else if (a % 5 == 0)
 		{
-			printf("%s ", "Buzz");
+			printf("%s ","Buzz");
 		}
-		else if (a % 3 == 0 && a % 5 == 0)
+		else if (a % 3 == 0 || a % 5 == 0)
 		{
-			printf("%s ", "FizzBuzz");
+			printf("%s ","FizzBuzz");
 		}
 		else
 			printf("%d ",a);
 	}
+	printf('\n');
 }
 
