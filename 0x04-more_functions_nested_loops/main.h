a832d9c68038dae51aced75f1c00856287aee760
ianmax32 <masagaian32@gmail.com>
2022-06-10 23:22:47 -0700

    Triangles

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -11,6 +11,7 @@ void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
 void print_diagonal(int n);
-void print_trianglr(int size);
+void print_triangle(int size);
 #endif /*MAIN_H*/
 void print_square(int size);
+void print_triangle(int size);
