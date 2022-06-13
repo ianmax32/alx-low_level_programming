0e98e49949916497555bb7e282ee186ddb3aaab3
ianmax32 <masagaian32@gmail.com>
2022-06-13 05:09:52 -0700

    strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strcpy - function coppied the string pointed from one
+ * pointer to another pointer.
+ *
+ * @dest:first argument parameteer representing destination
+ * @src:second argument parameter representing source
+ *
+ * Return: Always 0
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int a;
+
+	for (a = 0; src[a] >= 0; a++)
+	{
+		dest[a] = src[a];
+	}
+	return (dest);
+}
