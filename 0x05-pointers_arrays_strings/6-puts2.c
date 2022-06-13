74726de39e7f35b51319a02ef246a1891727d483
ianmax32 <masagaian32@gmail.com>
2022-06-13 04:13:34 -0700

    Half the lies they tell about me aren't true

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * puts2 - function prints every other character in a string
+ * @str: argument parameter to be used in the function
+ * Return:0
+ */
+
+void puts2(char *str)
+{
+	int a;
+	char b;
+
+	for (a = 0; str[a] != 0; a++)
+	{
+		if ((a % 2) == 0)
+			_putchar(str[a]);
+	}
+	_putchar('\n');
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -13,3 +13,4 @@ int _strlen(char *s);
 void _puts(char *str);
 void print_rev(char *s);
 void rev_string(char *s);
+void puts(char *str);
