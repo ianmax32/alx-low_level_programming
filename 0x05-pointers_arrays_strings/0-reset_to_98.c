89e5098df4e680fbc5c5f8112fb3c94043a25783
ianmax32 <masagaian32@gmail.com>
2022-06-13 03:08:01 -0700

    98 Battery st

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - function updates the value to 98
+ * @n: argument parameter to be used in the function as a pointer
+ * Return:0
+ */
+
+void return_to_98(int *n)
+{
+	*n = 98;
+	return (0);
+}
