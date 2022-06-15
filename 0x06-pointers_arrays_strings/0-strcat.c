cf53048572c17408021622e6cd93f673980e7f3a
ianmax32 <masagaian32@gmail.com>
2022-06-14 23:45:39 -0700

    strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strcat - function concatenates two strings
+ * @dest:first argumemt parameter to be used in the function
+ * @src:second argument to be used as the source in the function
+ * Return:0
+ */
+
+char *_strcat(char *dest, char*src)
+{
+	int i = 0;
+	int j = 0;
+
+	while (dest[a] != '\0')
+		i++;
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		a++;
+		b++;
+	}
+	return (dest);
+}
