ef3a14bef2e2425c75d090538d04bc92f3b1eda4
ianmax32 <masagaian32@gmail.com>
2022-06-15 11:03:09 -0700

    Always look up

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -3,18 +3,18 @@
 /**
  * string_toupper - function changes all the lowercase letters
  * to uppercase
- * @a:argument parameter representing pointer to the string
+ * @s:argument parameter representing pointer to the string
  * Return:0
  */
 
-char *string_toupper(char *)
+char *string_toupper(char *s)
 {
 	int a;
 
-	while (*[a] != '\0')
+	while (s[a] != '\0')
 	{
-		if (*[a] >= 'a' && *[a] <= 'z')
-			*[a] = *[a] - 32;
+		if (s[a] >= 'a' && s[a] <= 'z')
+			s[a] = s[a] - 32;
 		a++;
 	}
 	return (0);
