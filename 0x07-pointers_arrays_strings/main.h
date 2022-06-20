37df494e0d63ab7d9624334d3c20da55dce4a7cf
ianmax32 <masagaian32@gmail.com>
2022-06-20 14:26:31 -0700

    strchr

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -6,4 +6,4 @@
 char *_memset(char *s, char b, unsigned int n);
 #endif /*MAIN_H*/
 char *_memcpy(char *dest, char *src, unsigned int n);
-char *strchr(char *s, char c);
+char *_strchr(char *s, char c);
