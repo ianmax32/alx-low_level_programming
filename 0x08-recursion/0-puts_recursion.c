66400cbb186be143cdb859f045508817c320e84d
ianmax32 <masagaian32@gmail.com>
2022-06-22 13:41:56 -0700

    She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,20 @@
+#Include "main.h"
+
+/**
+ * _puts_recursion - function prints a string followed by a new line
+ * @s:argument parameter representing string to be printed
+ * Return:0
+ */
+
+void _puts_recursion(char *s)
+{
+	int a = 0;
+
+	if (s[a] == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	_put_recursion(s + 1);
+	_putchar(s[a]);
+}
