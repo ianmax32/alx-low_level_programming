1d24e5746ba93a38db2df8790f7470ac203188d0
ianmax32 <masagaian32@gmail.com>
2022-07-19 16:08:20 -0700

    Free

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -2,7 +2,7 @@
 #include "lists.h"
 
 /**
- *  free_listint2- function frees the list
+ * free_listint2- function frees the list
  * of the listint_t
  * @head:argument parameter representing list head
  * Return:0
@@ -13,7 +13,7 @@ void free_listint2(listint_t **head)
 	listint_t *node;
 
 	if (head == NULL)
-		return (NULL);
+		return;
 
 	while (*head)
 	{
