b92be65719e1b029d2b8bebf154d356ca8451bbb
ianmax32 <masagaian32@gmail.com>
2022-07-19 15:31:05 -0700

    Print list

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * print_listint - function prints all the elements
+ * of the listint_t
+ * @h:argument parameter representing list
+ * Return:0
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	size_t a;
+
+	a = 0;
+	while (h)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		a++;
+	}
+
+	return (a);
+}
