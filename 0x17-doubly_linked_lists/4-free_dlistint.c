29059d01b1435d140f4bfc0b25b4d7ffa626ecb5
ianmax32 <masagaian32@gmail.com>
2022-09-02 13:56:55 -0700

    Free list

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - function frees a lists
+ * @head:pointer to the gead of the lists
+ * Return:0
+ */
+
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t temp;
+
+	while (head)
+	{
+		temp = head;
+		head = head->next;
+		free(temp);
+	}
+}
