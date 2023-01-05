a4a1b23dd6332fa46dcac9ad1b2ff0233e0f1781
ianmax32 <masagaian32@gmail.com>
2023-01-05 01:20:49 -0800

    Linear search

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,24 @@
+#include "search_algos.h"
+/**
+ * this function searches for a value
+ * in an array of integers using
+ * linear search algorithm
+ */
+
+int linear_search(int *array, size_t size, int value)
+{
+	size_t a;
+
+	if(array == NULL)
+		return (-1);
+
+	for(a = 0; a < size; a++)
+	{
+		printf("Value checked array[%lu] = [%d]\n", a, array[a]);
+		if(array[a] == value)
+		{
+			return (a);
+		}
+	}
+	return (-1);
+}
---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,10 @@
+#ifndef _ALGORITHMS_H_
+#define _ALGORITHMS_H_
+
+#include "stdio.h"
+#include "stdlib.h"
+
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+
+#endif
